bc1185b127c1700aa2a3a909805b758eefe80832
Philip Tunbjer <philip.tunbjer@gmail.com>
2021-12-30 11:52:57 +0100

    time prints ok

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,18 @@
-def add_time(start, duration):
+def add_time(start, duration, week=None):
+    startArr = start.split(' ')
+    startTime = startArr[0].split(':')
+    ampm = startArr[1]
+
+    durationTime = duration.split(':')
+
+    endHour = (int(startTime[0]) + int(durationTime[0])) % 12
+    tmpEndMinute = (int(startTime[1]) + int(durationTime[1]))
+    if(tmpEndMinute > 59):
+        endHour += 1
+    
+    endMinute = tmpEndMinute % 60
 
 
+    endTime = str(endHour) + ':' + str(endMinute).zfill(2)
 
-
-
-    return new_time
+    return endTime
